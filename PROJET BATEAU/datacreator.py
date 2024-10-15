d088dac88606f07aeba66d45cac07b91cef0e7d5
DJMEL911 <145206807+DJMEL911@users.noreply.github.com>
2024-10-15 11:39:03 +0200

    Delete PROJET BATEAU directory

--- PROJET BATEAU/datacreator.py
+++
@@ -1,8 +0,0 @@
-﻿import sqlite3
-
-c = sqlite3.connect("data.db")
-
-cur = c.cursor()
-cur.execute("CREATE TABLE Bateau(id_bateau, position);")
-cur.execute("CREATE TABLE Mesures(id_bateau, temperature, Ph, date, heure);")
-cur.execute("CREATE TABLE Utilisateur(id_bateau, id_utilisateur);")
